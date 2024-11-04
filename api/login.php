379386a32337ffd28195ca780cfb836ddc78e92b
KenTaghap <109163116+KenTaghap@users.noreply.github.com>
2024-11-04 22:43:43 +0800

    Update login.php

--- api/login.php
+++ api/login.php
@@ -1,40 +1,35 @@
 <?php
 require 'vendor/autoload.php';
 
-use MongoDB\Client as MongoClient;
-
-// Enable error reporting for production
 error_reporting(E_ERROR | E_PARSE);
 
-$errorMsg = "";
+use MongoDB\Client as MongoClient;
 
-// Check if POST request is made
+// Connect to MongoDB Atlas
+try {
+    $mongoClient = new MongoClient("mongodb+srv://glycerasiado17:glycerasiado17@cluster0.s9v6t.mongodb.net/admin_login");
+    $database = $mongoClient->admin_login;
+    $collection = $database->users;
+} catch (Exception $e) {
+    die("Error connecting to MongoDB: " . $e->getMessage());
+}
+
+$errorMsg = ""; // Initialize an error message variable
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Connect to MongoDB
-    try {
-        $mongoClient = new MongoClient("mongodb+srv://glycerasiado17:glycerasiado17@cluster0.s9v6t.mongodb.net/admin_login");
-        $database = $mongoClient->admin_login;
-        $collection = $database->users;
-    } catch (Exception $e) {
-        die("Error connecting to MongoDB: " . $e->getMessage());
-    }
-
-    // Get username and password from POST data
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Query for the user in MongoDB
+    // Query for the user
     $query = ["username" => $username, "password" => $password];
     $user = $collection->findOne($query);
 
     if ($user) {
-        // Redirect to the dashboard on successful login
+        // Redirect or indicate successful login
         header("Location: ../home/index.html");
         exit();
     } else {
-        // Redirect back to the HTML page with an error message
-        header("Location: ../index.html?error=Invalid+username+or+password");
-        exit();
+        $errorMsg = "Invalid username or password.";
     }
 }
 ?>
